4352c809c084785c8f26b829d8160be85a921d14
Jevon Opel <jevonop88@gmail.com>
2022-06-23 11:13:17 +0700

    change minor

--- lib/value/constant_api.dart
+++ lib/value/constant_api.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 
 class ConstantAPI {
-  static const BASE_URL = kDebugMode ? "https://api.sewasam.com/v1/" : "https://api.sewasam.com/v1/";
+  static const BASE_URL = kDebugMode ? "https://blablabla/" : "https://blablabla/";
   static const HEADER_AUTH_TAG = "Authorization";
   static const HEADER_AUTH_KEY = "Bearer ";
   static const DURATION_TIME_OUT = Duration(seconds: 30);
